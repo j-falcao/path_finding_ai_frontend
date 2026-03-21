e980aaff22977115b824127afa8e8c63a1f2e051
Joao Falcao <joaorpalharesfalcao@gmail.com>
2026-03-21 20:16:26 +0100

    organizing project files and creating app layout

---
+++ src/components/layout/NavbarComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="navbar">
+    <router-link to="/">Map Search</router-link>
+    <router-link to="/editor">Graph Editor</router-link>
+    <router-link to="/algorithms">Algorithms</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MapSearch from '../pages/MapSearchPage.vue'
+import GraphEditorPage from '../pages/GraphEditorPage.vue'
+import AlgorithmsPage from '../pages/AlgorithmsPage.vue'
+import AboutPage from '../pages/AboutPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+    { path: "/", component: MapSearch },
+    { path: "/editor", component: GraphEditorPage },
+    { path: "/algorithms", component: AlgorithmsPage },
+    { path: "/about", component: AboutPage }
+  ]
 })
 
 export default router
