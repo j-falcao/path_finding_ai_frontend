8d25d1830fdb5cdb4851feaee8c1e8c0d68983af
Joao Falcao <joaorpalharesfalcao@gmail.com>
2026-03-21 21:57:37 +0100

    navbar

--- src/components/layout/NavbarComponent.vue
+++ src/components/layout/NavbarComponent.vue
@@ -1,8 +1,27 @@
 <template>
-  <nav class="navbar">
-    <router-link to="/">Map Search</router-link>
-    <router-link to="/editor">Graph Editor</router-link>
-    <router-link to="/algorithms">Algorithms</router-link>
-    <router-link to="/about">About</router-link>
+  <nav>
+    <div class="pages">
+      <router-link to="/">Map Search</router-link>
+      <router-link to="/editor">Graph Editor</router-link>
+      <router-link to="/algorithms">Algorithms</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <div class="license">Matrícula: 12-AB-34</div>
   </nav>
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  border-bottom: 1px solid #ccc;
+}
+.pages {
+  display: flex;
+  gap: 1rem;
+}
+.license {
+  font-weight: bold;
+}
+</style>
