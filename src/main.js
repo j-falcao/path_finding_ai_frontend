e1c9ad1a2ad1f62869a8295cd412aa53855e6f81
Joao Falcao <joaorpalharesfalcao@gmail.com>
2026-04-09 10:09:18 +0200

    adding ToastService to the app

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura';
+import ToastService from 'primevue/toastservice';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ToastService);
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
